537ca3e3a3065e4f2825df6bb1596192613a802c
timzzzy <tonan2acc@mail.ru>
2023-07-16 19:45:18 +0300

    APP VERSION 2.0 (WITH H2_DB)

--- src/main/java/com/example/test/MainView.java
+++ src/main/java/com/example/test/MainView.java
@@ -3,12 +3,12 @@ package com.example.test;
 import com.example.test.entity.Value;
 import com.example.test.repo.ValueRepo;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
 import java.util.List;
+import java.util.Objects;
 
 
 @Route("main")
@@ -20,17 +20,38 @@ public class MainView extends VerticalLayout {
 
     public MainView(ValueRepo repo) {
         this.valueRepo = repo;
-        List<Value> valueList = valueRepo.findAll();
-        value = valueList.get(0);
+        List<Value> valueList = this.valueRepo.findAll();
+        if(valueList.isEmpty()){
+            value = new Value();
+            value.setValue(0);
+        }
+        else{
+            value = valueList.get(0);
+        }
+        valueRepo.save(value);
         TextField textField = new TextField();
-        Button button = new Button("Hi");
+        Button button = new Button("PLUS ONE");
         add(button);
         add(textField);
-        button.addClickListener(buttonClickEvent -> value.setValue(value.getValue() + 1));
-        button.addClickListener(buttonClickEvent -> textField.setValue(String.valueOf(value.getValue())));
-        button.addClickListener(buttonClickEvent -> repo.save(value));
+        textField.setValue(String.valueOf(value.getValue()));
+        button.addClickListener(buttonClickEvent -> {
+            try {
+                if(Objects.equals(textField.getValue(), "")){
+                    value.setValue(0);
+                }
+                else if(!Integer.valueOf(textField.getValue()).equals(value.getValue())){
+                    value.setValue(Integer.valueOf(textField.getValue()));
+                }
+                else{
+                    value.setValue(value.getValue() + 1);
+                }
+                textField.setValue(String.valueOf(value.getValue()));
+                repo.save(value);
+            }
+            catch (Exception e){
+                textField.setValue("");
+                textField.setPlaceholder("Error");
+            }
+        });
     }
-
-
-
 }
--- src/main/java/com/example/test/entity/Value.java
+++ src/main/java/com/example/test/entity/Value.java
@@ -1,23 +1,22 @@
 package com.example.test.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "values")
+@Table(name = "VAL_TABLE")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Value {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
     public Integer id;
-    @Column(name = "value")
+    @Column(name = "VAL")
     public Integer value;
 }
